d1e7f6bdf1ecf382f677eb8035b7bda8846f684b
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-17 10:05:30 +0300

    added a list and detail view function

--- projectviews/views.py
+++ projectviews/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.http import HttpResponse
+from rest_framework.response import Response
+from .models import  Projects,Profile
+from django.views.generic import ListView,DetailView
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Projects
+    template_name = 'index.html'
+    context_object_name = 'projects'
+    ordering = ['-created_date']
+
+class PostDetailView(DetailView):
+    model = Projects
