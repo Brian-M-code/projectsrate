a380a2e6492536406c8ac3e99e7c524a24a1aecb
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-17 10:50:37 +0300

    added a create post view function

--- projectviews/views.py
+++ projectviews/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
 from rest_framework.response import Response
 from .models import  Projects,Profile
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 
 
 class PostListView(ListView):
@@ -13,3 +13,12 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Projects
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Projects
+    success_url = '/'
+    fields =['author','image','description', 'title' ,'link']
+
+    def form_valid(self, form):
+        form.instance.profile = self.request.user.profile
+        return super ().form_valid(form)
