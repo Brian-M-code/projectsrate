7cd01492e1791eb28148786bcf810a54b55ff94d
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-17 10:55:29 +0300

    modified url file

--- projectviews/urls.py
+++ projectviews/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.static import static
 from django.conf.urls import url,include
-from .views import PostListView,PostDetailView
+from .views import PostListView,PostDetailView, PostCreateView,PostUpdateView
 from . import views
 
 
@@ -8,4 +8,7 @@ urlpatterns = [
     url('',  PostListView.as_view(), name='projects-index'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^api/projects/$', views.ProjectsList.as_view()),
+    url(r'^post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    url(r'^profile/details/<str:username>/',views.display_profile, name = 'profile-detail'),
+    url(r'^post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 ]
