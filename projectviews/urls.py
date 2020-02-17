95b0bfb871b79547d362d9d1ee463666802d8073
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-17 11:21:06 +0300

    added post delete, create and review url confs

--- projectviews/urls.py
+++ projectviews/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.static import static
 from django.conf.urls import url,include
-from .views import PostListView,PostDetailView, PostCreateView,PostUpdateView
+from .views import PostListView,PostDetailView, PostCreateView,PostUpdateView,PostDeleteView,ReviewCreateView
 from . import views
 
 
@@ -11,4 +11,7 @@ urlpatterns = [
     url(r'^post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     url(r'^profile/details/<str:username>/',views.display_profile, name = 'profile-detail'),
     url(r'^post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    url(r'^post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    url(r'^post/new/$', PostCreateView.as_view(), name='post-create'),
+    url(r'^review/new/<int:pk>/', ReviewCreateView.as_view(), name='review-create'),
 ]
