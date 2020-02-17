a8071899f6656a40fa3a060b27c53cbc0fce2c1a
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-17 10:10:15 +0300

    added a profile model

--- projectviews/models.py
+++ projectviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='users/', default='user.png')
+    bio = models.TextField(default="Welcome!")
+
+    def __str__(self):
+        return f'{self.user.username} Profile' 
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
