fe16b5e5a235718d04cc24747912fdaa02b06dda
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-20 09:31:58 +0300

    added form file

---
+++ projectviews/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from projectviews.models import Projects,Profile
+from django.contrib.auth.forms import UserCreationForm
+
+class ProjectUploadForm(forms.ModelForm):
+    
+    class Meta:
+        model = Projects
+        fields = ('user', 'profile_picture', 'bio', 'link')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('design', 'usability', 'content')
